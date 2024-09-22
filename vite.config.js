10f6f9a78dd70d714c2cd383215be0088718efb2
kpierzynski <kpierzynski@hotmail.com>
2024-09-22 15:22:17 +0200

    add new .env approach in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@stores': path.resolve(__dirname, './src/stores'),
-      '@primitives': path.resolve(__dirname, './src/components/primitives'),
-      '@views': path.resolve(__dirname, './src/views'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log('process.env.VITE_BASEURL', env);
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@stores': path.resolve(__dirname, './src/stores'),
+        '@primitives': path.resolve(__dirname, './src/components/primitives'),
+        '@views': path.resolve(__dirname, './src/views'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
     },
-  },
-  base: process.env.VITE_BASEURL + '/',
+    base: env.VITE_BASEURL,
+  };
 });
